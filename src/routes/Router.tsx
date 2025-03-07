7df084fd9a1b624c50b5f50723108536fa5fc32a
Shubham Gagare <shubham.gagare@siyaratechin.com>
2025-03-07 21:42:27 +0530

    udpated

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -174,7 +174,7 @@ const Router = [
     path: '/',
     element: <FullLayout />,
     children: [
-      { path: '/', element: <Navigate to="/apps/ecommerce/shop" /> },
+      { path: '/', element: <Navigate to="/home/ecommerce/shop" /> },
       { path: '/dashboards/modern', exact: true, element: <ModernDash /> },
       { path: '/dashboards/ecommerce', exact: true, element: <EcommerceDash /> },
       { path: '/home/contacts', element: <Contacts /> },
